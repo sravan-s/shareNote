2f69994dd53dc685af6fd2e82eec2e20beb0cdcb
sravan-s <sravans2011@gmail.com>
2015-07-19 12:08:50 +0530

    Made a better layout structure

--- app/src/main/java/com/example/qasas/sharenote/notesl.java
+++ app/src/main/java/com/example/qasas/sharenote/notesl.java
@@ -6,18 +6,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class notesList extends ActionBarActivity {
+public class notesl extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notes_list);
+        setContentView(R.layout.activity_notes);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_notes_list, menu);
+        getMenuInflater().inflate(R.menu.menu_notes, menu);
         return true;
     }
 
